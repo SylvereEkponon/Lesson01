f9d5cbef4d6cd36db802fd09b4ec4cd9b5fee9a2
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-09-09 12:28:37 -0400

    Created Page_Load method

--- Lesson01/Default.aspx.cs
+++ Lesson01/Default.aspx.cs
@@ -5,13 +5,23 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+/*Name: Sylvere Ekponon
+ *Date: September 5, 2017
+ *Version 1: Page_Load method created
+ */ 
+
 namespace Lesson01
 {
     public partial class _Default : Page
     {
+        /// <summary>
+        /// This is a Page_Load method displays the today's date and time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            myTimeLabel.Text = DateTime.Now.ToString();
         }
     }
 }
